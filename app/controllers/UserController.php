3979f9976838e67a63dd8194c5db171d6b292e4e
Khim Lian Moldogo <khimlianmoldogo@gmail.com>
2026-09-05 19:17:32 +0800

    aiven lab4

---
+++ app/controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+
+/**
+ * Controller: UserController
+ * 
+ * Automatically generated via CLI.
+ */
+class UserController extends Controller {
+
+    public  function show_users()
+    {
+        $users = $this->UserModel->all();
+        ddt ($users, 'Users Tables');
+        $this->call->view('users');
+    }
+}
